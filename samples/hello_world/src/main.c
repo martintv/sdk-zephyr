05f966f98a5bb4d1f61f4e775174ba5b1b1817b1
Martin Tverdal <martin.tverdal@nordicsemi.no>
2023-10-30 11:59:27 +0100

    Small application that reproduced issue on appcore
    Fails with CONFIG_SOC_NRF53_ANOMALY_168_WORKAROUND=n
    And passes with CONFIG_SOC_NRF53_ANOMALY_168_WORKAROUND=y
    
    By failing I mean it will have crashed if you leave it runnign for a few secconds
    and connect with debugger afterwards.
    Important that you let it run without debugger attached, because connecting debugger
    prevents issue from occuring.
    
    When it has failed it will have hit a fatal error and callstack will look something like
    
    arch_system_halt (reason=reason@entry=35) at /home/martintv/repos/sdk-nrf/zephyr/kernel/fatal.c:32
    32              for (;;) {
    (gdb) bt
        at /home/martintv/repos/sdk-nrf/zephyr/arch/arm/core/aarch32/cortex_m/fault.c:1131
    Backtrace stopped: previous frame identical to this frame (corrupt stack?)

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -1,13 +1,54 @@
-/*
- * Copyright (c) 2012-2014 Wind River Systems, Inc.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
+#include "nrf.h"
 #include <zephyr/kernel.h>
+#include <stdint.h>
+
+uint32_t duration = 3;
+uint32_t timer0stop = 80;
+void mpsl_RTC0_isr_handler(void)
+{
+	NRF_RTC0->EVENTS_COMPARE[0] = 0;
+	NRF_RTC0->CC[0] = duration;
+	NRF_RTC0->TASKS_START = 1;
+	if (timer0stop == 120)
+	{
+		timer0stop = 80;
+	}
+	else
+	{
+		timer0stop++;
+	}
+	NRF_CLOCK->TASKS_HFCLKSTOP=1;
+	NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
+	NRF_TIMER0->TASKS_STOP = 1;
+	NRF_TIMER0->TASKS_CLEAR = 1;
+	NRF_TIMER0->CC[0] = timer0stop;
+	NRF_TIMER0->EVENTS_COMPARE[0] =0;
+	NRF_TIMER0->TASKS_START = 1;
+}
 
 int main(void)
 {
-	printk("Hello World! %s\n", CONFIG_BOARD);
+	NRF_CLOCK->PUBLISH_HFCLKSTARTED =  0 | (1 << 31);
+	NRF_CLOCK->SUBSCRIBE_HFCLKSTART =  10 | (1 << 31);
+
+	NRF_DPPIC->CHENSET = (1 << 10);
+	NRF_POWER->PUBLISH_SLEEPENTER =  2 | (1 << 31);
+	NRF_POWER->PUBLISH_SLEEPEXIT =  3 | (1 << 31);
+	IRQ_CONNECT(RTC0_IRQn, 0,mpsl_RTC0_isr_handler, 0, 0);
+	irq_enable(RTC0_IRQn);
+	NRF_RTC0->SHORTS = RTC_SHORTS_COMPARE0_CLEAR_Msk;
+	NRF_RTC0->CC[0] = duration;
+	NRF_RTC0->INTENSET =RTC_INTENSET_COMPARE0_Msk;
+	NRF_RTC0->EVTENSET =RTC_INTENSET_COMPARE0_Msk;
+	NRF_RTC0->TASKS_START = 1;
+	NRF_TIMER0->BITMODE = TIMER_BITMODE_BITMODE_32Bit;
+	NRF_TIMER0->SHORTS = TIMER_SHORTS_COMPARE0_STOP_Msk| TIMER_SHORTS_COMPARE0_CLEAR_Msk;
+	NRF_TIMER0->CC[0] = timer0stop;
+	NRF_TIMER0->PUBLISH_COMPARE[0] = (1 << 31) | 10;
+	NRF_TIMER0->TASKS_START = 1;
+	while(1)
+	{
+		k_sleep(K_MSEC(10000));
+	}
 	return 0;
 }
